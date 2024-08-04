eec729f2f5da968db8c4442833c999b16dac842d
Cleberton Francisco <clebertonfgc@gmail.com>
2024-08-03 22:22:01 -0300

    ajust to public class

--- CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
 {
-    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
