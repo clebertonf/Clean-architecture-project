a849f32c9b3fd02e82d742549e24f69251883afa
Cleberton Francisco <clebertonfgc@gmail.com>
2024-08-05 21:48:05 -0300

    add CategoryRepository

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _applicationDbContext = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _applicationDbContext.Categories.AddAsync(category);
+            await _applicationDbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _applicationDbContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _applicationDbContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _applicationDbContext.Categories.Remove(category);
+            await _applicationDbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _applicationDbContext.Categories.Update(category);
+            await _applicationDbContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
